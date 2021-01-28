8022668e3bf7acf066b7bda8136110c071d48a0b
Mikail <Mikailkalkanpro@gmail.com>
2021-01-28 01:54:38 +0100

    fix nav + Section Mik

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,13 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById('navbar').style.top = '0';
+    document.getElementById('custom-navbar').style.top = '0';
+    document.getElementById('custom-navbar').style.transition = 'top 1s';
   } else {
-    document.getElementById('navbar').style.top = '-100px';
+    document.getElementById('custom-navbar').style.top = '-100px';
+    document.getElementById('custom-navbar').style.transition = 'top 1s';
   }
   prevScrollpos = currentScrollPos;
 };
 
 // LIST TRI
-
-// var btnHtml = document.getElementById('html');
-// var btnCss = document.getElementById('css');
-// var btnJavaScript = document.getElementById('javascript');
-// var btnPhp = document.getElementById('php');
-// var btnBootstrap = document.getElementById('bootstrap');
-
-// btnHtml.addEventListener('click', sortTout);
-// btnCss.addEventListener('click', sortTout);
-// btnJavaScript.addEventListener('click', sortJavaScript);
-// // btnPhp.addEventListener('click', sortPhp);
-// // btnBootstrap.addEventListener('click', sortBootstrap);
-
-// var displayTout = document.getElementById('displayTout');
-
-// var htmlClass = document.getElementsByClassName('htmlC');
-// var cssClass = document.getElementsByClassName('cssC');
-// var javaScriptClass = document.getElementsByClassName('javaScriptC');
-// var phpClass = document.getElementsByClassName('phpC');
-// var bootstrapClass = document.getElementsByClassName('bootstrapC');
-
-// function sortTout() {
-//   if (displayTout.style.display == 'none') {
-//     displayTout.style.display = 'block';
-//   } else {
-//     displayTout.style.display = 'none';
-//   }
-// }
-
-// function sortJavaScript() {
-//   for (let i = 0; i < 30; i++) {
-//     if(displayTout.childNodes.htmlClass)
-//   }
-// }
